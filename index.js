3998db3cf7fa5c0293e8b5b6d88a79242b4e1425
SainiAjay1234 <ajaysaini334422@gmail.com>
2023-03-26 16:44:01 +0530

    404-featurei

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const http = require("http");
 
 http
    .createServer((req,res) => {
-       console.log(res);
-       res.write("<h1>Home Page1</h1>");
-       res.end();
+    switch (request.url) {
+        case "/":
+            response.writeHead(200, {
+                "Content-Type": "text/html",
+            });
+            response.write("<h1>Home Page1</h1>");
+            response.end();
+            break;
+        default:
+            response.writeHead(404, {
+                "Content-Type": "text/html",
+            });
+            response.write("<p>404 Page Not found1</p>");
+            response.end();
+            break;
+
+    }
+
+    
    })
    .listen(8080);
    
