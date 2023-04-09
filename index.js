f6e9102671dd84a96684920605614980b7ceadd8
SainiAjay1234 <ajaysaini334422@gmail.com>
2023-04-09 16:18:28 +0530

    about page created

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const http = require("http");
 
 http
    .createServer((req,res) => {
-       console.log(res);
-       res.write("<h1>Home Page1</h1>");
-       res.end();
+    switch (request.url) {
+        case "/":
+            response.writeHead(200, {
+                "Content-Type": "texthtml",
+            });
+            response.write("<h1>Home Page!</h1>");
+            response.end();
+            break;
+        case "/about":
+            response.writeHead(200, {
+                "Content-Type": "text/html",
+            });
+            response.write("<h2>About Page!</h2>");
+            response.end();
+            break;
+    }
    })
    .listen(8080);
    
